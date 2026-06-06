bc440a9230cc9ab6429422c24e498aed08b6bc13
Gaspar <e972136@gmail.com>
2026-06-06 15:21:48 -0600

    Lo que hay

---
+++ empleados-backEnd/src/main/java/com/gaspar/empleados/model/Empleado.java
@@ -0,0 +1,11 @@
+package com.gaspar.empleados.model;
+
+import java.time.LocalDate;
+
+public class Empleado {
+    Integer id;
+    String nombre;
+    String correo;
+    LocalDate fechaNacimiento;
+    Integer posicionId;
+}
---
+++ empleados-backEnd/src/main/java/com/gaspar/empleados/model/Posicion.java
@@ -0,0 +1,6 @@
+package com.gaspar.empleados.model;
+
+public class Posicion {
+    Integer id;
+    String nombre;
+}
